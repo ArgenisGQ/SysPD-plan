36001407f68fbf7fbbb90d7febe94c1d26921770
Argenis Gil <argenis.gq@gmail.com>
2023-10-30 10:46:10 -0400

    Dashboard:

--- src/views/admin/planning/forms/form01.jsx
+++ src/views/admin/planning/forms/form01.jsx
@@ -19,7 +19,14 @@ import {
   InputRightElement, 
   Grid,
   useColorModeValue,
-  Text  
+  Text,
+  Divider,
+  AbsoluteCenter,
+  Card,
+  CardHeader,
+  CardBody,
+  Stack,
+  StackDivider     
 } from '@chakra-ui/react'
 
 import { HSeparator } from "../../../../components/separator/Separator";
@@ -410,6 +417,48 @@ export default function Form01() {
             <Input id="hde" placeholder="HDE" />
           </FormControl>
         </Flex>
+
+        <Box position='relative' padding='10'>
+          <Divider />
+          {/* <AbsoluteCenter bg='white' px='4'>
+            Content
+          </AbsoluteCenter> */}
+        </Box>
+
+        <Card>
+  <CardHeader>
+    <Heading size='md'>Client Report</Heading>
+  </CardHeader>
+
+  <CardBody>
+    <Stack divider={<StackDivider />} spacing='4'>
+      <Box>
+        <Heading size='xs' textTransform='uppercase'>
+          Summary
+        </Heading>
+        <Text pt='2' fontSize='sm'>
+          View a summary of all your clients over the last month.
+        </Text>
+      </Box>
+      <Box>
+        <Heading size='xs' textTransform='uppercase'>
+          Overview
+        </Heading>
+        <Text pt='2' fontSize='sm'>
+          Check out the overview of your clients.
+        </Text>
+      </Box>
+      <Box>
+        <Heading size='xs' textTransform='uppercase'>
+          Analysis
+        </Heading>
+        <Text pt='2' fontSize='sm'>
+          See a detailed analysis of all your business clients.
+        </Text>
+      </Box>
+    </Stack>
+  </CardBody>
+</Card>
 
 
 
