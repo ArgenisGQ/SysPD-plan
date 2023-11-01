95b4e1a0b5cfe4b9481e14f7c20d326178d7a884
Argenis Gil <argenis.gq@gmail.com>
2023-11-01 08:21:13 -0400

    Dashboard: unidades de la asignatura

--- src/views/admin/planning/forms/form03.jsx
+++ src/views/admin/planning/forms/form03.jsx
@@ -18,7 +18,10 @@ import {
   FormHelperText,
   InputRightElement, 
   Grid,
-  useColorModeValue, 
+  useColorModeValue,
+  Card,
+  CardHeader,
+  CardBody, 
 } from '@chakra-ui/react'
 
 import InputField from '../../../../components/fields/InputField'
@@ -26,12 +29,225 @@ import InputField from '../../../../components/fields/InputField'
 import { useToast } from '@chakra-ui/react'
 
 export default function Form03() {
+    // Chakra color mode
+    const textColor = useColorModeValue("navy.700", "white");
+    const textColorSecondary = "gray.400";
+    const textColorDetails = useColorModeValue("navy.700", "secondaryGray.600");
+    const textColorBrand = useColorModeValue("brand.500", "white");
+    const brandStars = useColorModeValue("brand.500", "brand.400");    
+    const [show, setShow] = useState(false)
+    const handleClick = () => setShow(!show)
+    const [value, setValue] = useState('1')
     return (
       <>
         <Heading w="100%" textAlign={'center'} fontWeight="normal">
-          Ajustes de puntos de las unidades
+          Ajustes de puntos de las unidades 
         </Heading>
-        <SimpleGrid columns={1} spacing={6}>
+        <FormControl mt="2%">
+          <FormLabel 
+            htmlFor="course"
+            fontSize='sm' 
+            fontWeight={'normal'}
+            color={textColor}>
+            Nombre de la asignatura
+          </FormLabel>
+          <Input id="course" /* type="email" */ />
+          <FormHelperText color="subtle">Nombre asignado..</FormHelperText>
+          {/* <FormHelperText>We&apos;ll never share your email.</FormHelperText> */}
+        </FormControl>
+        <Flex>
+          <FormControl mt="2%" mr="5%">
+            <FormLabel
+              display='flex'
+              /* ms='4px' */
+              fontSize='sm'
+              fontWeight='500'
+              color={textColor}
+              /* mb='8px' */
+               
+              htmlFor="code-course" 
+              /* fontWeight={'normal'} */>
+              Codigo de Asignatura
+            </FormLabel>
+            <Input 
+              id="first-name" 
+              placeholder="Codigo de la asignatura" />
+          </FormControl>
+  
+          <FormControl mt="2%" mr="5%">
+            <FormLabel 
+              display='flex'
+              /* ms='4px' */
+              fontSize='sm'
+              fontWeight='500'
+              color={textColor}
+              /* mb='8px' */
+              htmlFor="seccion-course" 
+              /* fontWeight={'normal'} */>
+              Seccion
+            </FormLabel>
+            <Input id="last-name" placeholder="Seccion" />
+          </FormControl>
+
+          <FormControl mt="2%">
+            <FormLabel 
+              display='flex'
+              /* ms='4px' */
+              fontSize='sm'
+              fontWeight='500'
+              color={textColor}
+              /* mb='8px' */
+              htmlFor="units" 
+              /* fontWeight={'normal'} */>
+              Unidades de Asignatura
+            </FormLabel>
+            <Input id="last-name" placeholder="Unidades" />
+          </FormControl>
+        </Flex>
+
+        <Flex mt="5%">
+            <FormControl mt="2%" mr="4%">
+              <FormLabel
+                display='flex'
+                /* ms='4px' */
+                fontSize='sm'
+                fontWeight='500'
+                color={textColor}
+                /* mb='8px' */
+                
+                htmlFor="unit-1" 
+                /* fontWeight={'normal'} */>
+                Unidad 1
+              </FormLabel>
+              <Input 
+                id="unit-1" 
+                placeholder="%" />
+            </FormControl>
+    
+            
+            <FormControl mt="2%" mr="4%">
+              <FormLabel
+                display='flex'
+                /* ms='4px' */
+                fontSize='sm'
+                fontWeight='500'
+                color={textColor}
+                /* mb='8px' */
+                
+                htmlFor="unit-2" 
+                /* fontWeight={'normal'} */>
+                Unidad 2
+              </FormLabel>
+              <Input 
+                id="unit-2" 
+                placeholder="%" />
+            </FormControl>
+
+            <FormControl mt="2%" mr="4%">
+              <FormLabel
+                display='flex'
+                /* ms='4px' */
+                fontSize='sm'
+                fontWeight='500'
+                color={textColor}
+                /* mb='8px' */
+                
+                htmlFor="unit-3" 
+                /* fontWeight={'normal'} */>
+                Unidad 3
+              </FormLabel>
+              <Input 
+                id="unit-3" 
+                placeholder="%" />
+            </FormControl>
+
+            <FormControl mt="2%" mr="4%">
+              <FormLabel
+                display='flex'
+                /* ms='4px' */
+                fontSize='sm'
+                fontWeight='500'
+                color={textColor}
+                /* mb='8px' */
+                
+                htmlFor="unit-4" 
+                /* fontWeight={'normal'} */>
+                Unidad 4
+              </FormLabel>
+              <Input 
+                id="unit-4" 
+                placeholder="%" />
+            </FormControl>        
+          </Flex>
+
+          <Card
+            direction={{ base: 'column', sm: 'row' }}
+            mt="5%"
+            w='50%'
+            /* overflow='hidden' */
+            /* variant='outline' */>
+            <CardHeader>
+              <Heading size='md'>Total de puntos de la Asignatura</Heading>
+            </CardHeader>
+
+            <CardBody>
+            <Flex>
+                  <FormControl mt="2%" mr="1%" /* w='50%' */>
+                      <FormLabel
+                        display='flex'
+                        /* ms='4px' */
+                        fontSize='sm'
+                        fontWeight='500'
+                        color={textColor}
+                        /* mb='8px' */
+                        
+                        htmlFor="hours-total" 
+                        /* fontWeight={'normal'} */>
+                        %
+                      </FormLabel>
+                      <Input 
+                        id="hours-total" 
+                        placeholder="---"
+                        /* w='50%'  */
+                        /* size="sm" */ />
+                    </FormControl>
+            
+                                     
+                  </Flex>
+
+              {/* <Stack divider={<StackDivider />} spacing='4'>
+                <Box>
+                  <Heading size='xs' textTransform='uppercase'>
+                    Summary
+                  </Heading>
+                  <Text pt='2' fontSize='sm'>
+                    View a summary of all your clients over the last month.
+                  </Text>
+                </Box>
+                <Box>
+                  <Heading size='xs' textTransform='uppercase'>
+                    Overview
+                  </Heading>
+                  <Text pt='2' fontSize='sm'>
+                    Check out the overview of your clients.
+                  </Text>
+                </Box>
+                <Box>
+                  <Heading size='xs' textTransform='uppercase'>
+                    Analysis
+                  </Heading>
+                  <Text pt='2' fontSize='sm'>
+                    See a detailed analysis of all your business clients.
+                  </Text>
+                </Box>
+              </Stack> */}
+            </CardBody>
+          </Card>
+
+
+
+
+        {/* <SimpleGrid columns={1} spacing={6}>
           <FormControl as={GridItem} colSpan={[3, 2]}>
             <FormLabel
               fontSize="sm"
@@ -94,7 +310,7 @@ export default function Form03() {
               }}
             />                  
           </FormControl>
-        </SimpleGrid>
+        </SimpleGrid> */}
       </>
     )
   }
