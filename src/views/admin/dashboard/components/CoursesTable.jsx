1c9603950bced0af30b61badddfb963e945f5b3a
Argenis Gil <argenis.gq@gmail.com>
2023-11-13 09:12:06 -0400

    Dashboard:colocado paginacion a tabla de asignaturas

--- src/views/admin/dashboard/components/CoursesTable.jsx
+++ src/views/admin/dashboard/components/CoursesTable.jsx
@@ -11,7 +11,22 @@ import {
   Tr,
   useColorModeValue,
   TableContainer,
+  //-----//
+  IconButton,// 
+  Tooltip,
+  Select,
+  NumberInput,
+  NumberInputField,
+  NumberInputStepper,
+  NumberIncrementStepper,
+  NumberDecrementStepper,
 } from "@chakra-ui/react";
+import {
+  ArrowRightIcon,
+  ArrowLeftIcon,
+  ChevronRightIcon,
+  ChevronLeftIcon
+} from "@chakra-ui/icons";
 import React, { useMemo } from "react";
 import {
   useGlobalFilter,
@@ -49,6 +64,16 @@ export default function ColumnsTable(props) {
     page,
     prepareRow,
     initialState,
+    //-------//
+    canPreviousPage,
+    canNextPage,
+    pageOptions,
+    pageCount,
+    gotoPage,
+    nextPage,
+    previousPage,
+    setPageSize,
+    state: { pageIndex, pageSize }
   } = tableInstance;
   initialState.pageSize = 5;
 
@@ -60,8 +85,11 @@ export default function ColumnsTable(props) {
       w='100%'
       h='100%'
       px='0px'
-      /* overflowY="scroll" */      
+      /* overflowY="auto"  */ 
+      /* overflowY="auto" 
+      maxHeight="380px"   */  
       overflowX={{ sm: "scroll", lg: "hidden" }}>
+      
       {/* <TableContainer whiteSpace>  */}     
       <Flex px='25px' justify='space-between' mb='10px' align='center'>
         <Text
@@ -219,6 +247,89 @@ export default function ColumnsTable(props) {
           })}
         </Tbody>
       </Table>
+      {/* Paginacion */}
+      <Flex justifyContent="space-between" m={4} alignItems="center">
+        <Flex>
+          <Tooltip label="Primera página">
+            <IconButton
+              onClick={() => gotoPage(0)}
+              isDisabled={!canPreviousPage}
+              icon={<ArrowLeftIcon h={3} w={3} />}
+              mr={4}
+            />
+          </Tooltip>
+          <Tooltip label="Previa página">
+            <IconButton
+              onClick={previousPage}
+              isDisabled={!canPreviousPage}
+              icon={<ChevronLeftIcon h={6} w={6} />}
+            />
+          </Tooltip>
+        </Flex>
+
+        <Flex alignItems="center">
+          <Text flexShrink="0" mr={8}>
+            Page{" "}
+            <Text fontWeight="bold" as="span">
+              {pageIndex + 1}
+            </Text>{" "}
+            of{" "}
+            <Text fontWeight="bold" as="span">
+              {pageOptions.length}
+            </Text>
+          </Text>
+          <Text flexShrink="0">Ir a:</Text>{" "}
+          <NumberInput
+            ml={2}
+            mr={8}
+            w={28}
+            min={1}
+            max={pageOptions.length}
+            onChange={(value) => {
+              const page = value ? value - 1 : 0;
+              gotoPage(page);
+            }}
+            defaultValue={pageIndex + 1}
+          >
+            <NumberInputField />
+            <NumberInputStepper>
+              <NumberIncrementStepper />
+              <NumberDecrementStepper />
+            </NumberInputStepper>
+          </NumberInput>
+          <Select
+            w={32}
+            value={pageSize}
+            onChange={(e) => {
+              setPageSize(Number(e.target.value));
+            }}
+          >
+            {[10, 20, 30, 40, 50].map((pageSize) => (
+              <option key={pageSize} value={pageSize}>
+                Mostrar {pageSize}
+              </option>
+            ))}
+          </Select>
+        </Flex>
+
+        <Flex>
+          <Tooltip label="Siguiente página">
+            <IconButton
+              onClick={nextPage}
+              isDisabled={!canNextPage}
+              icon={<ChevronRightIcon h={6} w={6} />}
+            />
+          </Tooltip>
+          <Tooltip label="Ultima página">
+            <IconButton
+              onClick={() => gotoPage(pageCount - 1)}
+              isDisabled={!canNextPage}
+              icon={<ArrowRightIcon h={3} w={3} />}
+              ml={4}
+            />
+          </Tooltip>
+        </Flex>
+      </Flex>
       {/* </TableContainer> */}
     </Card>
   );
