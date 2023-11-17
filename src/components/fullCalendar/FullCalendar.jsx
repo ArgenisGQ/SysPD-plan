830e87954f05e0cc777aa3e7a08a33bb6903cd51
Argenis Gil <argenis.gq@gmail.com>
2023-11-17 12:55:28 -0400

    Dashboard: configurando fullcalender el idioma y las semanas

--- src/components/fullCalendar/FullCalendar.jsx
+++ src/components/fullCalendar/FullCalendar.jsx
@@ -3,18 +3,20 @@ import Calendar from "react-calendar";
 import "react-calendar/dist/Calendar.css";
 import "../../assets/css/MiniCalendar.css";
 import { Text, Icon } from "@chakra-ui/react";
-
+//FullCalendar
 import FullCalendar from '@fullcalendar/react'
 import dayGridPlugin from '@fullcalendar/daygrid'
 import interactionPlugin from "@fullcalendar/interaction";
 import { v4 as uuid } from "uuid";
+import esLocale from '@fullcalendar/core/locales/es';
 // Chakra imports
 import { MdChevronLeft, MdChevronRight } from "react-icons/md";
 // Custom components
 import Card from "../card/Card";
 
-export default function FullCalendarx(props) {
+export default  function FullCalendarx(props) {
   const [events, setEvents] = useState([]);
+  
   const handleSelect = (info) => {
     const { start, end } = info;
     const eventNamePrompt = prompt("Enter, event name");
@@ -38,10 +40,39 @@ export default function FullCalendarx(props) {
   };
   const WeekItem = ({ info }) => {
     const { event } = info;
+    const { week } = 1;    
     return (
-        <p>{event.title}</p>
+        {week}                       
     );
   };
+  const getCalendarViews = () => ({
+    resourceTimelineWeek: {
+      type: 'resourceTimeline',
+      duration: {
+        week: 1,
+      },
+    },
+    resourceDayGridWeek: {
+      type: 'resourceDayGrid',
+      duration: {
+        week: 3,
+      },
+    },
+    resourceTimelineMonth: {
+      type: 'resourceTimeline',
+      duration: {
+        month: 1,
+      },
+    },
+    resourceDayGridMonth: {
+      type: 'resourceDayGrid',
+      duration: {
+        month: 1,
+      },
+    },
+  });  
+  const views = getCalendarViews();
+  
   return (
     <FullCalendar
         /* BASE */
@@ -70,13 +101,21 @@ export default function FullCalendarx(props) {
         selectable
         events={events}
         select={handleSelect}
+        selectMirror
+        dayMaxEvents
+        firstDay={1}
+        views={views}
+        locale={esLocale}
+        fixedWeekCount={false} //cantidad de semanas por mes
+        weekText = ""
+        /* dayMinWidth={96} */
         /* columnFormat= {
           month: 'ddd',
           week: 'ddd',
           day: 'dddd M/d'
         } */
-        weekNumbers={true}
-        weekNumberCalculation={-40}
+        /* weekNumbers={true}  */ //numero de semanas del año
+        /* weekNumberCalculation={(info) => <WeekItem info={info} />} */
         headerToolbar={{
           start: "today prev next",
           end: "dayGridMonth dayGridWeek dayGridDay",
@@ -84,7 +123,7 @@ export default function FullCalendarx(props) {
         eventContent={(info) => <EventItem info={info} />}        
         /* eventContent={console.log(events)} */
         plugins={[dayGridPlugin, interactionPlugin]}
-        views={["dayGridMonth", "dayGridWeek", "dayGridDay"]}
+        /* views={["dayGridMonth", "dayGridWeek", "dayGridDay"]} */
       />
   )
 
@@ -112,4 +151,67 @@ export default function FullCalendarx(props) {
       />
     </Card>
   ); */
+}
+
+function FullCalendary(props) {
+  const getCalendarViews = () => ({
+    resourceTimelineWeek: {
+      type: 'resourceTimeline',
+      duration: {
+        week: 1,
+      },
+    },
+    resourceDayGridWeek: {
+      type: 'resourceDayGrid',
+      duration: {
+        week: 3,
+      },
+    },
+    resourceTimelineMonth: {
+      type: 'resourceTimeline',
+      duration: {
+        month: 1,
+      },
+    },
+    resourceDayGridMonth: {
+      type: 'resourceDayGrid',
+      duration: {
+        month: 1,
+      },
+    },
+  });
+  
+  const views = getCalendarViews();
+  
+  <FullCalendar
+          ref={ref}
+          editable
+          selectable
+          selectMirror
+          dayMaxEvents
+          firstDay={1}
+          views={views}
+          events={events}
+          dayMinWidth={96}
+          plugins={plugins}
+          resources={resources}
+          headerToolbar={false}
+          resourceAreaWidth="240px"
+          slotDuration={{ days: 1 }}
+          eventStartEditable={false}
+          datesAboveResources={false}
+          eventDurationEditable={false}
+          eventResizableFromStart={false}
+          initialView={CalendarGridView.HORIZONTAL_WEEK}
+          schedulerLicenseKey={FULL_CALENDAR_LICENSE_KEY}
+          filterResourcesWithEvents={hideEmptyResourcesModel.value}
+          slotLabelContent={slot => <CalendarDateCell slot={slot} />}
+          eventContent={e => <RentEvent fcEvent={e} events={rentals} />}
+          resourceLabelContent={args => <CalendarResourceLabel label={args.resource.title} />}
+          resourceAreaColumns={[
+            {
+              headerContent: <CalendarResourceHeader productSection={productsSection} />,
+            },
+          ]}
+  />
 }
