e406f42e2af47d7064f9f3a341bfa27ce87b8659
Argenis Gil <argenis.gq@gmail.com>
2023-11-27 11:55:27 -0400

    Dashboard: formato 03, seleccion de 3 a 4 unidades, primera parte

--- src/views/admin/planning/forms/form03.jsx
+++ src/views/admin/planning/forms/form03.jsx
@@ -37,7 +37,23 @@ export default function Form03() {
     const brandStars = useColorModeValue("brand.500", "brand.400");    
     const [show, setShow] = useState(false)
     const handleClick = () => setShow(!show)
-    const [value, setValue] = useState('1')
+    const [value, setValue] = useState(null)
+    const [valueC1, setValueC1] = useState("unit01")
+    const [valueC2, setValueC2] = useState("unit02")
+    const [valueC3, setValueC3] = useState("unit03")
+    const [valueC4, setValueC4] = useState("unit03")
+    const [valueSel, setValueSel] = useState("4")
+    const unitsxx = (unidades) => {
+      if (!unidades) {
+        return <>
+          <Center mt="5%">
+            <p>Seleccione la UNIDAD</p>
+          </Center>            
+        </>
+        
+      }
+      return <Unit unitsx={unidad} others="50"/>};
+
     return (
       <>
         <Heading w="100%" textAlign={'center'} fontWeight="normal">
@@ -113,15 +129,136 @@ export default function Form03() {
               htmlFor="units" 
               /* fontWeight={'normal'} */>
               Unidades de Asignatura
-            </FormLabel>
+            </FormLabel>  
             <Input 
               id="last-name" 
               placeholder="Unidades"
-              defaultValue="Post"
+              defaultValue={valueSel}
               focusBorderColor="brand.400"
               color={textColorSecondary} />
+              
+              <Heading as='h6' size='xs' mt="10%">
+              Unidades de prueba de referencia
+              </Heading>
+              <Select placeholder='Cantidad de Unidades'
+                    value={ valueSel } onChange={ (event) => setValueSel( event.target.value ) }>
+                <option value='3'>3</option>
+                <option value='4'>4</option>                            
+              </Select>
+
           </FormControl>
         </Flex>
+
+        <Flex mt="5%">
+            <FormControl mt="2%" mr="4%">
+              <FormLabel
+                display='flex'
+                /* ms='4px' */
+                fontSize='sm'
+                fontWeight='500'
+                color={textColor}
+                /* mb='8px' */
+                
+                htmlFor="unit-1" 
+                /* fontWeight={'normal'} */>
+                Corte 1
+              </FormLabel>
+              {/* <Input 
+                id="unit-1" 
+                placeholder="%"
+                defaultValue="Post"
+                focusBorderColor="brand.400"
+                color={textColorSecondary} /> */}
+              <Select placeholder='Seleccione UNIDAD'
+                    value={ valueC1 } onChange={ (event) => setValueC1( event.target.value ) }>
+                <option value='unit01'>Unidad I</option>
+                <option value='unit02'>Unidad II</option>
+                <option value='unit03'>Unidad III</option>              
+              </Select>
+            </FormControl>
+    
+            
+            <FormControl mt="2%" mr="4%">
+              <FormLabel
+                display='flex'
+                /* ms='4px' */
+                fontSize='sm'
+                fontWeight='500'
+                color={textColor}
+                /* mb='8px' */
+                
+                htmlFor="unit-2" 
+                /* fontWeight={'normal'} */>
+                Corte 2
+              </FormLabel>
+              {/* <Input 
+                id="unit-2" 
+                placeholder="%"
+                defaultValue="Post"
+                focusBorderColor="brand.400"
+                color={textColorSecondary} /> */}
+              <Select placeholder='Seleccione UNIDAD'
+                    value={ valueC2 } onChange={ (event) => setValueC2( event.target.value ) }>
+                <option value='unit01'>Unidad I</option>
+                <option value='unit02'>Unidad II</option>
+                <option value='unit03'>Unidad III</option>              
+              </Select>
+            </FormControl>
+
+            <FormControl mt="2%" mr="4%">
+              <FormLabel
+                display='flex'
+                /* ms='4px' */
+                fontSize='sm'
+                fontWeight='500'
+                color={textColor}
+                /* mb='8px' */
+                
+                htmlFor="unit-3" 
+                /* fontWeight={'normal'} */>
+                Corte 3
+              </FormLabel>
+              {/* <Input 
+                id="unit-3" 
+                placeholder="%"
+                defaultValue="Post"
+                focusBorderColor="brand.400"
+                color={textColorSecondary} /> */}
+              <Select placeholder='Seleccione UNIDAD'
+                    value={ valueC3 } onChange={ (event) => setValueC3( event.target.value ) }>
+                <option value='unit01'>Unidad I</option>
+                <option value='unit02'>Unidad II</option>
+                <option value='unit03'>Unidad III</option>              
+              </Select>
+            </FormControl>
+
+            <FormControl mt="2%" mr="4%">
+              <FormLabel
+                display='flex'
+                /* ms='4px' */
+                fontSize='sm'
+                fontWeight='500'
+                color={textColor}
+                /* mb='8px' */
+                
+                htmlFor="unit-4" 
+                /* fontWeight={'normal'} */>
+                Corte 4
+              </FormLabel>
+              {/* <Input 
+                id="unit-4" 
+                placeholder="%"
+                defaultValue="Post"
+                focusBorderColor="brand.400"
+                color={textColorSecondary} /> */}
+              <Select placeholder='Seleccione UNIDAD'
+                    value={ valueC4 } onChange={ (event) => setValueC4( event.target.value ) }>
+                <option value='unit01'>Unidad I</option>
+                <option value='unit02'>Unidad II</option>
+                <option value='unit03'>Unidad III</option>              
+              </Select>
+            </FormControl>        
+          </Flex>
 
         <Flex mt="5%">
             <FormControl mt="2%" mr="4%">
