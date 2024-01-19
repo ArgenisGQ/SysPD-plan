9ff73fda6a7c6730ba4254212bab2a7846f1761d
Argenis Gil <argenis.gq@gmail.com>
2024-01-19 10:48:12 -0400

    Dashboad: Creado el modal de editar actividad para formulario 3

--- src/views/admin/planning/forms/modals/activityForm.jsx
+++ src/views/admin/planning/forms/modals/activityForm.jsx
@@ -30,17 +30,14 @@ import {
     Select  
   } from '@chakra-ui/react'
 
-import ActivityForm from './modals/activityForm';
-
-const UnitFiels = (props) => {
-    const { unitsx, others } = props;
+const ActivityForm = (props) => {
+    const { unit, activity } = props;
     /* const { isOpen, onOpen, onClose } = useDisclosure() */
     /* const { isOpenACtivities, onOpen, onClose } = useDisclosure() */
 
     const modalCreate = useDisclosure();
     const modalEdit = useDisclosure();
     const modalDelete = useDisclosure();
-    const modalActivityEdit = useDisclosure();
 
     const initialRef = useRef(null)
     const finalRef = useRef(null)
@@ -50,9 +47,6 @@ const UnitFiels = (props) => {
 
     const initialRefDelete = useRef(null)
     const finalRefDelete = useRef(null)
-
-    const initialRefActivityEdit = useRef(null)
-    const finalRefActivityEdit = useRef(null)
 
     /* const sizes = 'full' */
     const sizes = '6xl'
@@ -65,47 +59,8 @@ const UnitFiels = (props) => {
     const brandStars = useColorModeValue("brand.500", "brand.400");
     
     const [value, setValue] = useState(null);
-    const [valueEdit, setValueEdit] = useState(null);
-    const activitySelect = (activity) => {
-      if (!activity) {
-        return <>
-          <Center mt="5%">
-            <p>Seleccione la Actividad</p>
-          </Center>            
-        </>
-        
-      }
-      return <ActivityForm unit="1" activity="3"/>};      
+    const [valueEdit, setValueEdit] = useState(null);      
   return (<>
-    <Button onClick={modalCreate.onOpen} height='48px' width='450px'>Crear</Button>
-    <Button onClick={modalEdit.onOpen} height='48px' width='450px'>Editar</Button>
-    <Button onClick={modalDelete.onOpen} height='48px' width='450px'>Borrar</Button>
-      {/* <Button ml={4} ref={finalRef}>
-        I'll receive focus on close
-      </Button> */}
-
-      <Modal
-        initialFocusRef={initialRef}
-        finalFocusRef={finalRef}
-        isOpen={modalCreate.isOpen}
-        onClose={modalCreate.onClose}
-        size={sizes}
-        scrollBehavior={scrollBehavior}
-      >
-        <ModalOverlay />
-        <ModalContent>
-          <ModalHeader>Completar los datos</ModalHeader>
-          <ModalCloseButton />
-          <ModalBody pb={6}>
-            {/* <FormControl>
-              <FormLabel>Estrategia de Evaluacion</FormLabel>
-              <Input ref={initialRef} placeholder='--' />
-            </FormControl>
-
-            <FormControl mt={4}>
-              <FormLabel>Instrumento de Evaluacion</FormLabel>
-              <Input placeholder='--' />
-            </FormControl> */}
             <SimpleGrid spacing={4} templateColumns='repeat(auto-fill, minmax(380px, 1fr))' mt="5%" >
               <FormControl  mt="2%" id="proposito" mr="2%">
                 <FormLabel
@@ -228,117 +183,7 @@ const UnitFiels = (props) => {
                 {/* <FormHelperText color="subtle">Escriba el proposito de la asignatura</FormHelperText> */}
               </FormControl>          
             </SimpleGrid>
-          </ModalBody>
-
-          <ModalFooter>
-            <Button colorScheme='blue' mr={3}>
-              Guardar
-            </Button>
-            <Button onClick={modalCreate.onClose}>Cancelar</Button>
-          </ModalFooter>
-        </ModalContent>
-      </Modal>
-
-      {/* ------------------------------------------------------------------------------- */}
-
-      <Modal
-        initialFocusRef={initialRefActivities}
-        finalFocusRef={finalRefActivities}
-        isOpen={modalEdit.isOpen}
-        onClose={modalEdit.onClose}
-        size={sizes}
-        scrollBehavior={scrollBehavior}>
-        <ModalOverlay/>
-        <ModalContent>
-          <ModalHeader>Seleccionar actividad a editar</ModalHeader>
-          <ModalCloseButton />
-          <ModalBody pb={6}>
-              {/* <Center>
-                <h1>Actividad a editar</h1>
-              </Center>  */}              
-              <Center mt="1%">               
-                <Stack spacing={3} mt="2%"  w='30%' mr="5%">
-                  {/* <Select variant='outline' placeholder='Outline' /> */}
-                  {/* <Select variant='filled' placeholder='Filled' /> */}
-                  {/* <Select variant='flushed' placeholder='Flushed' /> */}
-                  {/* <Select variant='unstyled' placeholder='Unstyled' /> */}
-
-                  <Select variant='flushed' placeholder='Seleccione la Unidad'
-                          value={ valueEdit } onChange={ (event) => setValueEdit( event.target.value ) }>
-                    <option value='1'>Actividad 1</option>
-                    <option value='2'>Actividad 2</option>
-                    <option value='3'>Actividad 3</option>                    
-                  </Select>
-                </Stack>
-              </Center>
-              {console.log({valueEdit})}
-              {activitySelect(valueEdit)}
-          </ModalBody>
-          <ModalFooter>
-           {/*  <Button onClick={modalActivityEdit.onOpen} colorScheme='blue' mr={3}>
-              Seleccion aa
-            </Button>  */}
-            <Button onClick={modalActivityEdit.onOpen} colorScheme='blue' mr={3}>
-              Guardar
-            </Button>  
-            <Button onClick={modalEdit.onClose}>Cancelar</Button>
-          </ModalFooter>
-        </ModalContent>        
-      </Modal>
-
-      {/* ------------------------------------------------------------------------------- */}
-
-      <Modal
-        initialFocusRef={initialRefDelete}
-        finalFocusRef={finalRefDelete}
-        isOpen={modalDelete.isOpen}
-        onClose={modalDelete.onClose}
-        size={sizes}
-        scrollBehavior={scrollBehavior}>
-        <ModalOverlay/>
-        <ModalContent>
-          <ModalHeader>Seleccionar actividad a editar</ModalHeader>
-          <ModalCloseButton />
-          <ModalBody pb={6}>
-              <h1>lista de actividades para borrar</h1>
-          </ModalBody>
-          <ModalFooter>
-            <Button colorScheme='blue' mr={3}>
-              Seleccion
-            </Button>
-            <Button onClick={modalDelete.onClose}>Cancelar</Button>
-          </ModalFooter>
-        </ModalContent>        
-      </Modal>
-
-      {/* ------------------------------------------------------------------------------- */}
-      {/* DONT USED */}
-      <Modal
-        initialFocusRef={initialRefActivityEdit}
-        finalFocusRef={finalRefActivityEdit}
-        isOpen={modalActivityEdit.isOpen}
-        onClose={modalActivityEdit.onClose}
-        size="sm"
-        scrollBehavior={scrollBehavior}>
-        <ModalOverlay/>
-        <ModalContent>
-          <ModalHeader>EDITAR ACTIVIDAD</ModalHeader>
-          <ModalCloseButton />
-          <ModalBody pb={6}>
-              <Center>
-                <h1>lista de actividades para editar</h1>
-              </Center>  
-          </ModalBody>
-          <ModalFooter>
-            <Button  colorScheme='blue' mr={3}>
-              Guardar
-            </Button>            
-            <Button onClick={modalActivityEdit.onClose}>Cancelar</Button>
-          </ModalFooter>
-        </ModalContent>        
-      </Modal>
-
   </>)
 }
 
-export default UnitFiels
+export default ActivityForm
