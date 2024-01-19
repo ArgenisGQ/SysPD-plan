9664bfaadad1f9cde9ac8def3d1b785d55037226
Argenis Gil <argenis.gq@gmail.com>
2024-01-19 14:14:20 -0400

    Dashboard:

--- src/views/admin/planning/forms/modals/activityForm.jsx
+++ src/views/admin/planning/forms/modals/activityForm.jsx
@@ -31,7 +31,12 @@ import {
     Editable,
     EditableInput,
     EditableTextarea,
-    EditablePreview,  
+    EditablePreview,
+    NumberInput,
+    NumberInputField,
+    NumberInputStepper,
+    NumberIncrementStepper,
+    NumberDecrementStepper,  
   } from '@chakra-ui/react'
 
 const ActivityForm = (props) => {
@@ -63,7 +68,13 @@ const ActivityForm = (props) => {
     const brandStars = useColorModeValue("brand.500", "brand.400");
     
     const [value, setValue] = useState(null);
-    const [valueEdit, setValueEdit] = useState(null);      
+    const [valueEdit, setValueEdit] = useState(null);
+    const [valueWeek, setValueWeek] = useState(null);
+    
+    const format = (val) => `% ` + val
+    const parse = (val) => val.replace(/^\$/, '')
+    const [valuePoints, setValuePoints] = useState(0)
+
   return (<>
             <SimpleGrid spacing={4} templateColumns='repeat(auto-fill, minmax(380px, 1fr))' mt="5%" >
               <FormControl  mt="2%" id="proposito" mr="2%">
@@ -154,7 +165,6 @@ const ActivityForm = (props) => {
                 </CardHeader>
 
                 <CardBody>
-
                     <RadioGroup onChange={setValue} value={value}>
                       <Stack direction='row'>
                         <Radio value='1'>Diagnostica</Radio>
@@ -175,10 +185,21 @@ const ActivityForm = (props) => {
                 color={textColor}>
                   Lapso/Entrega
                 </FormLabel>
-                <Textarea rows={4} resize="none"
+                {/* <Textarea rows={4} resize="none"
                   defaultValue="Post"
                   focusBorderColor="brand.400"
-                  color={textColorSecondary} />
+                  color={textColorSecondary} /> */}
+
+                <RadioGroup defaultValue='1' mt="2%" ml="4%"
+                /* onChange={(event) => setValuePre( event)} */
+                value={valueWeek} onChange={ (event) => setValueWeek(event) }>
+                  <Stack>
+                    <Radio value='1' /* isDisabled */>Semana 1</Radio>                    
+                    <Radio value='2'>Semana 2</Radio>
+                    <Radio value='3'>Semana 3</Radio>
+                  </Stack>
+                </RadioGroup>
+                {console.log({valueWeek})}
                 {/* <FormHelperText color="subtle">Escriba el proposito de la asignatura</FormHelperText> */}
               </FormControl>
               <FormControl  mt="2%" id="proposito" /* mr="2%" */>
@@ -189,10 +210,53 @@ const ActivityForm = (props) => {
                 color={textColor}>
                   Ponderacion/Calificacion
                 </FormLabel>
-                <Textarea rows={4} resize="none"
+                {/* <Textarea rows={4} resize="none"
                   defaultValue="Post"
                   focusBorderColor="brand.400"
-                  color={textColorSecondary} />
+                  color={textColorSecondary} /> */}
+                <FormControl mt="2%" mr="4%">
+                  {/* <FormLabel
+                    display='flex'
+                    
+                    fontSize='sm'
+                    fontWeight='500'
+                    color={textColor}
+                    
+                    
+                    htmlFor="unit-4" 
+                    >
+                    Porcentaje
+                  </FormLabel> */}
+                  {/* <Input 
+                    id="unit-4" 
+                    placeholder="%"
+                    defaultValue="Post"
+                    focusBorderColor="brand.400"
+                    color={textColorSecondary} /> */}
+                      
+                </FormControl>
+                      <NumberInput 
+                        /* defaultValue={25}  */
+                        size='lg' 
+                        precision={0} 
+                        step={1}
+                        min={0} 
+                        max={30}
+                        onChange={(valueString) => setValuePoints(parse(valueString))}
+                        value={format(valuePoints)}
+                        focusBorderColor="brand.400"
+                        color={textColorSecondary}
+                        /* color='gray.300' */
+                        /* keepWithinRange={false} */
+                        /* clampValueOnBlur={false} */>
+                        {/* {console.log("corte 4-2: ", parseInt(valueCor4))} */}
+                        {console.log({valuePoints})}
+                        <NumberInputField />
+                        <NumberInputStepper>
+                          <NumberIncrementStepper />
+                          <NumberDecrementStepper />
+                        </NumberInputStepper>
+                      </NumberInput>
                 {/* <FormHelperText color="subtle">Escriba el proposito de la asignatura</FormHelperText> */}
               </FormControl>          
             </SimpleGrid>
